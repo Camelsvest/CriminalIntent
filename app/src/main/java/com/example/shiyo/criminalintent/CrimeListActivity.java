60215aabe2c415c3b5cd83dae466d52b42da75c9
camelsvest <2945791313@qq.com>
2017-10-28 23:33:35 +0800

    Finished Chapter 9

--- app/src/main/java/com/example/shiyo/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/shiyo/criminalintent/CrimeListActivity.java
@@ -2,11 +2,13 @@ package com.example.shiyo.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by shiyo on 2017/10/27.
+ */
 
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        //return new CrimeFragment();
-        return null;
+        return new CrimeListFragment();
     }
 }
