675dfba93acd625c2b581e115f72dc73675deca1
camelsvest <2945791313@qq.com>
2017-11-02 23:36:07 +0800

    Going on chapter 13

--- app/src/main/java/com/example/shiyo/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/shiyo/criminalintent/SingleFragmentActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.shiyo.criminalintent.CrimeFragment;
 import com.example.shiyo.criminalintent.R;
@@ -12,7 +13,8 @@ import com.example.shiyo.criminalintent.R;
  * Created by shiyo on 2017/10/27.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+//public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
     @Override
